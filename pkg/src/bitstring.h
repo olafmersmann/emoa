6702a29280c28e46555ad854be6de1d73017f1e3
Olaf Mersmann <olafm@statistik.tu-dortmund.de>
2009-07-29 00:02:35 +0200

    Change inline to R_INLINE
    Package is not necessarily compiled with a C99 compliant compiler. Use
    R_INLINE macro instead of inline keyword to mark bitstring functions
    suitable for inlining.

--- pkg/src/bitstring.h
+++ pkg/src/bitstring.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#include <R.h> /* for R_INLINE */
+
 typedef uint32_t bs_base_t;
 
 typedef struct {
@@ -13,36 +15,36 @@ typedef struct {
 
 const size_t bs_base_size = sizeof(bs_base_t);
 
-static inline void bitstring_initialize(bitstring_t *bs, size_t size) {
+static R_INLINE void bitstring_initialize(bitstring_t *bs, size_t size) {
     bs->size = size;
     bs->string = (uint32_t *)malloc(size / bs_base_size + 1);
     memset(bs->string, 0, size / bs_base_size + 1);
     return bs;
 }
 
-static inline void bitstring_delete(bitstring_t bs) {
+static R_INLINE void bitstring_delete(bitstring_t bs) {
     free(bs.string);
 }
 
-static inline void bitstring_set(bitstring_t bs, size_t el) {
+static R_INLINE void bitstring_set(bitstring_t bs, size_t el) {
     size_t i = el / bs_base_size;
     bs_base_t mask = 1 << (el % bs_base_size);
     bs.string[i] |= mask;
 }
 
-static inline void bitstring_clear(bitstring_t bs, size_t el) {
+static R_INLINE void bitstring_clear(bitstring_t bs, size_t el) {
     size_t i = el / bs_base_size;
     bs_base_t mask = 1 << (el % bs_base_size);
     bs.string[i] &= !mask;
 }
 
-static inline int bitstring_is_set(const bitstring_t bs, size_t el) {
+static R_INLINE int bitstring_is_set(const bitstring_t bs, size_t el) {
     size_t i = el / bs_base_size;
     bs_base_t mask = 1 << (el % bs_base_size);
     return ((bs.string[i] & mask) != 0);
 }
 
-static inline int bitstring_is_clear(const bitstring_t bs, size_t el) {
+static R_INLINE int bitstring_is_clear(const bitstring_t bs, size_t el) {
     return !bitstring_is_set(bs, el);
 }
 
