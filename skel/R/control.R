dc53618bbb6c59c2a5235e75145f7bb549ca87be
Olaf Mersmann <olafm@statistik.tu-dortmund.de>
2010-05-11 19:40:17 +0200

    Add preliminary documentation to *_control() functions.

--- skel/R/control.R
+++ skel/R/control.R
@@ -5,16 +5,28 @@
 ##
 
 ##' Basic EMOA control parameters.
-##' 
-##' Recognized control parameters:
+##'
+##' The following control parameters are recognized by \code{emoa_control}:
 ##' \describe{
 ##'   \item{logger}{\code{emoa_logger} object used to log events.}
 ##'   \item{n}{Number of parameters, defaults to the length of the longer
 ##'     of \code{upper} or \code{lower}.}
 ##'   \item{d}{Number of dimensions.}
 ##' }
+##'
+##' @param f Multiobjectve optimization function.
+##' @param upper Upper bounds of parameter space.
+##' @param lower Lower bounds of parameter space.
+##' @param ... Further arguments passed to \code{f}.
+##' @param control List of control parameters.
+##' @param default List of default control parameters.
+##'
+##' @return The \code{control} list with suitably adjusted
+##' arguments. Missing control parameters are taken from
+##' \code{default} or, if not present there, from an internal default.
 ##' 
 ##' @export
+##' @author Olaf Mersmann \email{olafm@@statistik.tu-dortmund.de}
 emoa_control <- function(f, upper, lower, ..., control, default) {
   control$logger <- coalesce(control[["logger"]], default[["logger"]], emoa_console_logger())
   control$n <- as.integer(coalesce(control[["n"]],
@@ -26,15 +38,28 @@ emoa_control <- function(f, upper, lower, ..., control, default) {
   control
 }
 
-##' Steady state EMOA control parameters.
+##' Steady state EMOA parameters
 ##' 
-##' Recognized control parameters:
+##' \code{steady_state_emoa_control} interprets the following control
+##' parameters:
 ##' \describe{
-##'   \item{mu}{Size of population.}
-##'   \item{maxeval}{Maximal number of function evaluations.}
+##'   \item{mu}{Population size.}
+##'   \item{maxeval}{Maximum number of function evaluations to use.}
 ##' }
 ##'
+##' @param f Multiobjectve optimization function.
+##' @param upper Upper bounds of parameter space.
+##' @param lower Lower bounds of parameter space.
+##' @param ... Further arguments passed to \code{f}.
+##' @param control List of control parameters.
+##' @param default List of default control parameters.
+##'
+##' @return The \code{control} list with suitably adjusted
+##' arguments. Missing control parameters are taken from
+##' \code{default} or, if not present there, from an internal default.
+##' 
 ##' @export
+##' @author Olaf Mersmann \email{olafm@@statistik.tu-dortmund.de}
 steady_state_emoa_control <- function(f, upper, lower, ..., control, default=list()) {
   control <- emoa_control(f, upper, lower, ..., control=control, default=default)
   control$mu <- as.integer(coalesce(control[["mu"]],
@@ -46,7 +71,29 @@ steady_state_emoa_control <- function(f, upper, lower, ..., control, default=lis
   control
 }
 
+##' Simulated binary crossover (SBX) control parameters
+##' 
+##' \code{sbx_control} interprets the following parameters used to
+##' control the behaviour of the simulated binary crossover operator
+##' (see \code{\link{sbx_operator}}):
+##' \describe{
+##'   \item{sbx.n}{Nu parameter of SBX.}
+##'   \item{sbx.p}{$p$ parameter of SBX.}
+##' }
+##'
+##' @param f Multiobjectve optimization function.
+##' @param upper Upper bounds of parameter space.
+##' @param lower Lower bounds of parameter space.
+##' @param ... Further arguments passed to \code{f}.
+##' @param control List of control parameters.
+##' @param default List of default control parameters.
+##'
+##' @return The \code{control} list with suitably adjusted
+##' arguments. Missing control parameters are taken from
+##' \code{default} or, if not present there, from an internal default.
+##' 
 ##' @export
+##' @author Olaf Mersmann \email{olafm@@statistik.tu-dortmund.de}
 sbx_control <- function(f, upper, lower, ..., control, default=list()) {
   if (!"crossover" %in% names(control)) {
     if (!"crossover" %in% names(default)) {
@@ -60,7 +107,27 @@ sbx_control <- function(f, upper, lower, ..., control, default=list()) {
   control
 }
 
+##' Polynomial muation (PM) control parameters
+##'
+##' Control parameters:
+##' \describe{
+##'   \item{pm.n}{Nu parameter of PM.}
+##'   \item{pm.p}{p parameter of PM.}
+##' }
+##' 
+##' @param f Multiobjectve optimization function.
+##' @param upper Upper bounds of parameter space.
+##' @param lower Lower bounds of parameter space.
+##' @param ... Further arguments passed to \code{f}.
+##' @param control List of control parameters.
+##' @param default List of default control parameters.
+##'
+##' @return The \code{control} list with suitably adjusted
+##' arguments. Missing control parameters are taken from
+##' \code{default} or, if not present there, from an internal default.
+##' 
 ##' @export
+##' @author Olaf Mersmann \email{olafm@@statistik.tu-dortmund.de}
 pm_control <- function(f, upper, lower, ..., control, default=list()) {
   if (!"mutate" %in% names(control)) {
     if (!"mutate" %in% names(default)) {
