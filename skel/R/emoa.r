2339bfcbf790ef2c62e8ef60f99c140581d33b1d
Olaf Mersmann <olafm@statistik.tu-dortmund.de>
2010-05-14 22:11:38 +0200

    Package documentation for release.

--- skel/R/emoa.r
+++ skel/R/emoa.r
@@ -1,2 +1,34 @@
+##'
+##' This package provides functions to construct evolutionary
+##' multiobjective optimization algorithms (EMOA). The long term goal
+##' is to also provide standard implementations of the most common
+##' EMOA in use today.
+##'
+##' Without the hard work of many researchers who have published their
+##' source code under a liberal license, this package would not ave
+##' been possible. In alphabetical order they are
+##'
+##' \itemize{
+##'   \item Michael H. Buselli
+##'   \item Wessel Dankers
+##'   \item Carlos Fonseca
+##'   \item Joshua Knowles
+##'   \item Huang Ling
+##'   \item Wudong Liu
+##'   \item Manuel Lopez-Ibanez
+##'   \item Luis Paquete
+##'   \item Ponnuthurai Nagaratnam Suganthany
+##'   \item Santosh Tiwar
+##'   \item Qingfu Zhang
+##'   \item Aimin Zhou
+##'   \item Shizheng Zhaoy
+##' }
+##' 
+##' @docType package
+##' @name emoa-package
+##' @title The EMOA package
+##' @author Olaf Mersmann \email{olafm@@statistik.tu-dortmund.de}
+##' @keywords package
 ##' @useDynLib emoa
-NULL
+
+NA
